eff37e63c2cb1e354cde79425f71dc89e91a1802
Kseniya <kseniyacherepovich@mail.ru>
2023-07-04 10:32:49 +0300

    \\\

---
+++ Lesson_5/lesson_5_2.py
@@ -0,0 +1,13 @@
+number_1 = int(input("Введите первое число: "))
+number_2 = int(input("Введите второе число: "))
+a = input("Выберите знак (+, -, *, /): ")
+if a == "+":
+    print(number_1 + number_2)
+elif a == "-":
+    print(number_1 - number_2)
+elif a == "*":
+    print(number_1 * number_2)
+elif a == "/":
+    print(number_1 / number_2)
+else:
+    print("Введено неверное значение")
