ca9ae02308f471cd61e0f1e48abc044db76cb8e7
Kseniya <kseniyacherepovich@mail.ru>
2023-07-04 11:02:14 +0300

    \\\

--- Lesson_5/lesson_5_2.py
+++ Lesson_5/lesson_5_2.py
@@ -1,6 +1,6 @@
 number_1 = int(input("Введите первое число: "))
 number_2 = int(input("Введите второе число: "))
-a = input("Выберите знак (+, -, *, /): ")
+a = input("Выберите знак (+, -, *, /, %): ")
 if a == "+":
     print(number_1 + number_2)
 elif a == "-":
@@ -9,5 +9,7 @@ elif a == "*":
     print(number_1 * number_2)
 elif a == "/":
     print(number_1 / number_2)
+elif a == "%":
+    print(number_1 % number_2)
 else:
     print("Введено неверное значение")
